9edf2ca59afc465bf75dcd5f1441ad23aac70760
yegemberdin <yegemberdin@gmail.com>
2019-12-01 22:19:35 +0600

    auth animation added

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-auth',
+  animations: [
+    trigger('EnterLeave', [
+      state('flyIn', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.8s 200ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
